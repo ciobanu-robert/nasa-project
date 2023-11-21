c897da153aedf4fc10969390a637762b6474d5ce
Robi <ciobanurobert100@gmail.com>
2023-11-21 14:57:51 +0200

    .

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,10 +7,23 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:deFG4G3l3D33j3Aq@nasacluster.r1x61yl.mongodb.net/nasa?retryWrites=true&w=majority'
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
 
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     await loadPlanetsData();
     
     server.listen(PORT, () => {
